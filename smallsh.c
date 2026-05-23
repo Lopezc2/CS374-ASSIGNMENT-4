1e71124a497d85c9e73b0909a75a5985677dc499
Christopher Lopez <lopezc9@oregonstate.edu>
2026-05-23 05:02:28 +0000

    Notes for headers, added flags, data structure, updated foreground-only mode fo ctrl-z

--- smallsh.c
+++ smallsh.c
@@ -16,12 +16,36 @@
 #define MAX_ARGS    512
 #define MAX_BG_PIDS 512
 
-/*Signals*/
+/*Flags to toggle SIGTSTP*/
+volatile sig_atomic_t fgOnlyMode = 0;
 
+/*Data structure*/
+typedef struct {
+    char *args[MAX_ARGS + 1]; // Command arguments
+    int argc;                 // Number of arguments
+    char *inputFile;          // Input redirection
+    char *outputFile;         // Output redirection
+    int background;           // Run in background
+} Command;
+
+/*Signals*/
 // Interrupt signal
 void handle_SIGINT(int sig) {
-    
     (void)sig;
+}
+
+// foreground-only for Ctrl-Z
+void handle_SIGTSTP(int sig) {
+    (void)sig;
+    if (!fgOnlyMode) {
+        fgOnlyMode = 1;
+        char *msg = "\nEntering forground-only mode (& is now ignored)\n";
+        write(STDOUT_FILENO, msg, 52);
+    } else {
+        fgOnlyMode = 0;
+        char *msg = "\nExiting foreground-only mode\n: ";
+        write (STDOUT_FILENO, msg, 32);
+    }
 }
 
 /*Main*/
