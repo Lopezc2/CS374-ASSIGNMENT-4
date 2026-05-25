f9dce6795b834e6da5456a0db7cb9e24de3d7d93
Christopher Lopez <lopezc9@oregonstate.edu>
2026-05-25 21:50:35 +0000

    AHHHHH

--- smallsh.c
+++ smallsh.c
@@ -1,6 +1,8 @@
 // Christopher Lopez
 // Assignment 4
-// Description: 
+// Description: Prompts, foreground/background execution, I/O redirection
+//              Built-in: Exit, cd, status
+//              Signal handling
 
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h> // PRINT, GET FFLSUH
@@ -28,24 +30,24 @@ pid_t bg_pids[MAX_BG_PIDS];
 int bg_pid_count = 0;
 
 /*Parse command*/
-typedef struct {
+struct command {
     char *args[MAX_ARGS + 1]; // Command arguments
     int argc;                 // Number of arguments
-    char *inputFile;          // Input redirection
-    char *outputFile;         // Output redirection
+    char *inFile;          // Input redirection
+    char *outFile;         // Output redirection
     int background;           // Run in background
-} Command;
+};
 
 /*Forward declarations */
 
 void setup_signals(void);
 void handle_SIGTSTP(int sig);
-void parse_command(char *line, Command *cmd);
+void parse_command(char *line, struct command *cmd);
 void reap_background_children(void);
 void run_builtin_exit(void);
-void run_builtin_cd(Command *cmd);
+void run_builtin_cd(struct command *cmd);
 void run_builtin_status(void);
-void run_external(Command *cmd);
+void run_external(struct command *cmd);
 
 /*Signals*/
 /*Signal setup*/
@@ -81,36 +83,36 @@ void handle_SIGTSTP(int sig) {
     } else {
         fgOnlyMode = 0;
         const char *msg = "\nExiting foreground-only mode\n";
-        write (STDOUT_FILENO, msg, strlen(msg));
+        write(STDOUT_FILENO, msg, strlen(msg));
     }
 }
 
 /*Parse*/
-void parse_command(char *line, Command *cmd) {
+void parse_command(char *line, struct command *cmd) {
     char *saveptr;
     char *token = strtok_r(line, " \t", &saveptr);
 
     while (token != NULL) {
         if (strcmp(token, "<") == 0) {
             token = strtok_r(NULL, " \t", &saveptr);
-            if (token) cmd->inputFile = token;
+            if (token) (*cmd).inFile = token;
         } else if (strcmp(token, ">") == 0) {
             token = strtok_r(NULL, " \t", &saveptr);
-            if (token) cmd-> outputFile = token;
+            if (token) (*cmd).outFile = token;
         } else {
-            cmd->args[cmd->argc++] = token;
+            (*cmd).args[(*cmd).argc++] = token;
         }
         token = strtok_r(NULL, " \t", &saveptr);
     }
-    cmd->args[cmd->argc] = NULL;
+    (*cmd).args[(*cmd).argc] = NULL;
 
-    if (cmd->argc > 0 && strcmp(cmd->args[cmd->argc - 1], "&") == 0) {
-        cmd-> background = 1;
-        cmd->args[--cmd->argc] = NULL;
+    if ((*cmd).argc > 0 && strcmp((*cmd).args[(*cmd).argc - 1], "&") == 0) {
+        (*cmd).background = 1;
+        (*cmd).args[--(*cmd).argc] = NULL;
     }
 
     if (fgOnlyMode) {
-        cmd->background = 0;
+        (*cmd).background = 0;
     }
 }
 
@@ -132,14 +134,14 @@ void reap_background_children(void) {
 /*BUILT-IN COMMANDS*/
 void run_builtin_exit(void) {
     // Kill all background children before exit
-    for (int i = 0; i < bg_pid_count; i ++) {
+    for (int i = 0; i < bg_pid_count; i++) {
         kill(bg_pids[i], SIGTERM);
     }
     exit(0);
 }
 
-void run_builtin_cd(Command *cmd) {
-    const char *path = (cmd->argc >= 2) ? cmd->args[1] : getenv("HOME");
+void run_builtin_cd(struct command *cmd) {
+    const char *path = ((*cmd).argc >= 2) ? (*cmd).args[1] : getenv("HOME");
     if (chdir(path) != 0) {
         perror("cd");
     }
@@ -155,7 +157,7 @@ void run_builtin_status(void) {
 }
 
 /*External commands*/
-void run_external(Command *cmd) {
+void run_external(struct command *cmd) {
     pid_t pid = fork();
 
     if (pid < 0) {
@@ -164,7 +166,7 @@ void run_external(Command *cmd) {
     }
 
     if (pid == 0) {
-        if (!cmd->background) {
+        if (!(*cmd).background) {
             struct sigaction sa = {0};
             sa.sa_handler = SIG_DFL;
             sigemptyset(&sa.sa_mask);
@@ -176,13 +178,13 @@ void run_external(Command *cmd) {
         sigemptyset(&sa_tstp.sa_mask);
         sigaction(SIGTSTP, &sa_tstp, NULL);
 
-        // INput redirection
-        const char *Infile = cmd->inputFile;
-        if (!Infile && cmd->background) Infile = "/dev/null";
-        if (Infile) {
-            int fd = open(Infile, O_RDONLY);
+        // Input redirection
+        const char *inFile = (*cmd).inFile;
+        if (!inFile && (*cmd).background) inFile = "/dev/null";
+        if (inFile) {
+            int fd = open(inFile, O_RDONLY);
             if(fd < 0) {
-                fprintf(stderr, "cannot open %s for input\n", Infile);
+                fprintf(stderr, "cannot open %s for input\n", inFile);
                 exit(1);
             }
             dup2(fd, STDIN_FILENO);
@@ -190,25 +192,25 @@ void run_external(Command *cmd) {
         }
 
         // Output redirection
-        const char *OutFile = cmd->outputFile;
-        if (!OutFile && cmd->background) OutFile = "/dev/null";
-        if (OutFile) {
-            int fd = open(OutFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (fd <0) {
-                fprintf(stderr, "cannot open %s for output\n", OutFile);
+        const char *outFile = (*cmd).outFile;
+        if (!outFile && (*cmd).background) outFile = "/dev/null";
+        if (outFile) {
+            int fd = open(outFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0) {
+                fprintf(stderr, "cannot open %s for output\n", outFile);
                 exit(1);
             }
             dup2(fd, STDOUT_FILENO);
             close(fd);
         }
-        execvp(cmd->args[0], cmd->args);
+        execvp((*cmd).args[0], (*cmd).args);
 
-        fprintf(stderr, "%s: no such file or directory\n", cmd->args[0]);
+        fprintf(stderr, "%s: no such file or directory\n", (*cmd).args[0]);
         exit(1);
     }
 
     // Parent process
-    if (cmd->background) {
+    if ((*cmd).background) {
         printf("background pid is %d\n", pid);
         fflush(stdout);
         // Track PID
@@ -224,7 +226,7 @@ void run_external(Command *cmd) {
             last_signal_status = 0;
         } else if (WIFSIGNALED(wstatus)) {
             last_fg_status = WTERMSIG(wstatus);
-            last_signal_status  = 1;
+            last_signal_status = 1;
             printf("terminated by signal %d\n", last_fg_status);
             fflush(stdout);
         }
@@ -258,7 +260,7 @@ int main(void) {
             continue;
         }
         
-        Command cmd;
+        struct command cmd;
         memset(&cmd, 0, sizeof(cmd));
         parse_command(line, &cmd);
 
